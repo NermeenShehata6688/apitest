96713b3aa981105e32cfda66443b221d8733fb19
MariamMikha <93828071+MariamMikha@users.noreply.github.com>
2021-12-30 09:00:30 +0300

    Mariam
    Ipes Pagination

--- IesSchool.Core/Services/IepService.cs
+++ IesSchool.Core/Services/IepService.cs
@@ -114,6 +114,14 @@ namespace IesSchool.Core.Services
               
 
                 var lstIepDto = _mapper.Map<List<VwIepDto>>(allIeps);
+                if (iepSearchDto.Index == null || iepSearchDto.Index == 0)
+                {
+                    iepSearchDto.Index = 0;
+                }
+                else 
+                {
+                    iepSearchDto.Index += 1;
+                }
                 var mapper = new PaginateDto<VwIepDto> { Count = allIeps.Count(), Items = lstIepDto != null ? lstIepDto.Skip(iepSearchDto.Index == null || iepSearchDto.PageSize == null ? 0 : ((iepSearchDto.Index.Value - 1) * iepSearchDto.PageSize.Value)).Take(iepSearchDto.PageSize ??= 20).ToList() : lstIepDto.ToList() };
 
                 //var mapper = new PaginateDto<VwIepDto> { Count = allIeps.Count(), Items = lstIepDto != null ? lstIepDto.Skip(iepSearchDto.Index ??= 0).Take(iepSearchDto.PageSize ??= 20).ToList() : lstIepDto.ToList() };
