befd622387dd60954c790a18ed95cf588397e1bf
nermeenshehata6688 <nermeenshehata6688@gmail.com>
2022-01-12 10:21:30 +0300

    1

--- IesSchool.Context/Models/IepProgressReport.cs
+++ IesSchool.Context/Models/IepProgressReport.cs
@@ -22,6 +22,8 @@ namespace IesSchool.Context.Models
         public string? OtherComment { get; set; }
         public int? TeacherId { get; set; }
         public int? HeadOfEducationId { get; set; }
+        public int? GoalShortTermNumber { get; set; }
+        public int? GoalLongTermNumber { get; set; }
         public DateTime? CreatedOn { get; set; }
         public DateTime? DeletedOn { get; set; }
         public bool? IsDeleted { get; set; }
