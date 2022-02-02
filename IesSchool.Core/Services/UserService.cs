df1eeb50eaae75764f28b06a9a11603f6d7f29d9
nermeenshehata6688 <nermeenshehata6688@gmail.com>
2022-02-02 09:29:45 +0300

    1

--- IesSchool.Core/Services/UserService.cs
+++ IesSchool.Core/Services/UserService.cs
@@ -207,7 +207,7 @@ namespace IesSchool.Core.Services
                 .Include(x => x.UserExtraCurriculars).ThenInclude(x => x.ExtraCurricular)
                 .Include(x => x.StudentExtraTeachers)
                 .Include(x => x.AspNetUser));
-                user.ImageBinary = null;
+                user.ImageBinary = null;       
                 var mapper = _mapper.Map<UserDto>(user);
                
                 var appUser = _userManager.Users.FirstOrDefault(r => r.Email == user.Email);
