6f39b599774329fe05d8d43ac5b1385e044f0e1c
nermeenshehata6688 <nermeenshehata6688@gmail.com>
2022-02-09 12:12:43 +0300

    1

--- IesSchool.Context/Models/Assistant.cs
+++ IesSchool.Context/Models/Assistant.cs
@@ -19,6 +19,7 @@ namespace IesSchool.Context.Models
         public int? DepartmentId { get; set; }
         public DateTime? CreatedOn { get; set; }
         public DateTime? DeletedOn { get; set; }
+        public DateTime? BirthDay { get; set; }
         public bool? IsDeleted { get; set; }
         public string? DeletedBy { get; set; }
         public string? CreatedBy { get; set; }
